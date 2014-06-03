a73920eec682624b2d5646ae53376bf61e228d48
Kraiss <rassat.pierrick@gmail.com>
2014-06-03 22:36:51 +0200

    Finition du tableau (sauf detail)

--- public/js/ogameApp.ang.js
+++ public/js/ogameApp.ang.js
@@ -25,7 +25,7 @@ angular
                 id:"Rasen", infuza:"4016631",
                 rank:101, holliday:false,
                 fleet: {
-                    size: 205, rank:76,
+                    size: 211, rank:76,
                     small:1033, large:893, recycler:390,
                     light:5044, heavy:868, cruiser:903,
                     battleship:750, destroyer:91,
@@ -55,23 +55,6 @@ angular
                 }
             },
 
-            {name:"ExO_Dus",
-                id:"ExO_Dus", infuza:"3963983",
-                rank:216, holliday:false,
-                fleet: {
-                    size: 107, rank:136,
-                    small:2146, large:190, recycler:108,
-                    light:1406, heavy:197, cruiser:416,
-                    battleship:468, destroyer:83,
-                    bomber:165, traqueur:160
-                },
-                tech: {
-                   weapon:0,
-                   shield:0,
-                   shell :0
-                }
-            },
-
             {name:"Thel Vadamee",
                 id:"Thel Vadamee", infuza:"3964184",
                 rank:271, holliday:false,
@@ -89,11 +72,28 @@ angular
                 }
             },
 
+            {name:"ExO_Dus",
+                id:"ExO_Dus", infuza:"3963983",
+                rank:216, holliday:false,
+                fleet: {
+                    size: 110, rank:136,
+                    small:2146, large:190, recycler:108,
+                    light:1406, heavy:197, cruiser:416,
+                    battleship:468, destroyer:83,
+                    bomber:165, traqueur:160
+                },
+                tech: {
+                   weapon:0,
+                   shield:0,
+                   shell :0
+                }
+            },
+
             {name:"kevv",
                 id:"kevv", infuza:"4165945",
                 rank:432, holliday:false,
                 fleet: {
-                    size: 84, rank:169,
+                    size: 86, rank:169,
                     small:1300, large:0, recycler:0,
                     light:472, heavy:0, cruiser:121,
                     battleship:4, destroyer:0,
@@ -106,11 +106,11 @@ angular
                 }
             },
 
-            {name:"Furious Biker",
-                id:"Furious Biker", infuza:"3588159",
-                rank:149, holliday:false,
+            {name:"bonux44",
+                id:"bonux44", infuza:"4135990",
+                rank:210, holliday:false,
                 fleet: {
-                    size: 44, rank:306,
+                    size: 55, rank:233,
                     small:0, large:0, recycler:0,
                     light:0, heavy:0, cruiser:0,
                     battleship:0, destroyer:0,
@@ -140,40 +140,6 @@ angular
                 }
             },
 
-            {name:"Etwhal",
-                id:"Etwhal", infuza:"3589263",
-                rank:95, holliday:false,
-                fleet: {
-                    size: 355, rank:39,
-                    small:0, large:0, recycler:0,
-                    light:0, heavy:0, cruiser:0,
-                    battleship:0, destroyer:0,
-                    bomber:0, traqueur:0
-                },
-                tech: {
-                    weapon:0,
-                    shield:0,
-                    shell :0
-                }
-            },
-
-            {name:"bonux44",
-                id:"bonux44", infuza:"4135990",
-                rank:210, holliday:false,
-                fleet: {
-                    size: 55, rank:233,
-                    small:0, large:0, recycler:0,
-                    light:0, heavy:0, cruiser:0,
-                    battleship:0, destroyer:0,
-                    bomber:0, traqueur:0
-                },
-                tech: {
-                    weapon:0,
-                    shield:0,
-                    shell :0
-                }
-            },
-
             {name:"S00999",
                 id:"S00999", infuza:"3753152",
                 rank:395, holliday:false,
@@ -187,7 +153,41 @@ angular
                 tech: {
                     weapon:8,
                     shield:8,
-                    shell :10,
+                    shell :10
+                }
+            },
+
+            {name:"Furious Biker",
+                id:"Furious Biker", infuza:"3588159",
+                rank:149, holliday:false,
+                fleet: {
+                    size: 44, rank:306,
+                    small:0, large:0, recycler:0,
+                    light:0, heavy:0, cruiser:0,
+                    battleship:0, destroyer:0,
+                    bomber:0, traqueur:0
+                },
+                tech: {
+                    weapon:0,
+                    shield:0,
+                    shell :0
+                }
+            },
+
+            {name:"Etwhal",
+                id:"Etwhal", infuza:"3589263",
+                rank:95, holliday:false,
+                fleet: {
+                    size: 39, rank:0,
+                    small:0, large:0, recycler:0,
+                    light:0, heavy:0, cruiser:0,
+                    battleship:0, destroyer:0,
+                    bomber:0, traqueur:0
+                },
+                tech: {
+                    weapon:0,
+                    shield:0,
+                    shell :0
                 }
             }
         ];
