f07cc51f38343814738716fae7fe0ee4208b7f53
Kraiss <rassat.pierrick@gmail.com>
2014-07-21 15:33:33 +0200

    Change font style

--- public/js/jlogApp.ang.js
+++ public/js/jlogApp.ang.js
@@ -7,13 +7,10 @@ angular
                 {
                     title: {fr: "Constructeur dynamique" ,en:"Dynamic Constructor"},
                     article: {
-                        fr: "C'est un test",
-                        en :"this is a test"
+                        fr: "C'est un test <a href='#'>link</a>",
+                        en :"<html><head></head><body>C'est un test <a href='#'>link</a></body></html>"
                     },
                     image:null
                 }
             ];
-
-
-
     });
