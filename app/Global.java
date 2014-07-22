3c4184b934bf6415e56f1855db2b2eb18500b45e
Kraiss <rassat.pierrick@gmail.com>
2014-07-22 10:22:47 +0200

    install mongodb (bugged ??)

---
+++ app/Global.java
@@ -0,0 +1,51 @@
+import com.google.code.morphia.Morphia;
+import com.mongodb.Mongo;
+import play.api.mvc.RequestHeader;
+import play.api.mvc.SimpleResult;
+import play.libs.F.Promise;
+import play.*;
+import play.mvc.*;
+import play.mvc.Http.*;
+import play.libs.F.*;
+import services.MorphiaObject;
+import play.Logger;
+import java.net.UnknownHostException;
+
+public class Global extends play.GlobalSettings {
+/*
+    public Promise<SimpleResult> onHandlerNotFound(RequestHeader request) {
+        return Promise.<SimpleResult>pure(notFound(
+                views.html.notFoundPage.render("error")
+        ));
+    }
+*/
+    @Override
+    public void onStart(play.Application arg0) {
+        super.beforeStart(arg0);
+        Logger.debug("** onStart **");
+
+        int port = 53479;
+        String url = "ds053479.mongolab.com";
+        String db = "heroku_app25530398";
+        String password = "KraissPlay";
+        String username = "KraissPlay";
+
+        try {
+            MorphiaObject.mongo = new Mongo(url, port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        MorphiaObject.morphia = new Morphia();
+        if(heroku) {
+            MorphiaObject.datastore = MorphiaObject.morphia.createDatastore(MorphiaObject.mongo, db, username, password.toCharArray());
+        }
+        else {
+            MorphiaObject.datastore = MorphiaObject.morphia.createDatastore(MorphiaObject.mongo, db);
+        }
+        MorphiaObject.datastore.ensureIndexes();
+        MorphiaObject.datastore.ensureCaps();
+
+        Logger.debug("** Morphia datastore: " + MorphiaObject.datastore.getDB());
+    }
+}
--- build.sbt
+++ build.sbt
@@ -2,8 +2,20 @@ name := "KraissPlay"
 
 version := "1.0-SNAPSHOT"
 
+resolvers += Resolver.url("SBT Plugin Releases", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
+
+resolvers += "Maven repository" at "http://morphia.googlecode.com/svn/mavenrepo/"
+
+resolvers += "MongoDb Java Driver Repository" at "http://repo1.maven.org/maven2/org/mongodb/mongo-java-driver/"
+
 libraryDependencies ++= Seq(
-	"org.webjars" %% "webjars-play" % "2.2.0", 
-	"org.webjars" % "bootstrap" % "2.3.1")
+  javaJdbc,
+  javaEbean,
+  cache,
+  "com.google.code.morphia" % "morphia" % "0.99",
+  "org.mongodb" % "mongo-java-driver" % "2.7.3",
+  "com.google.code.morphia" % "morphia-logging-slf4j" % "0.99",
+  "org.webjars" %% "webjars-play" % "2.2.0",
+  "org.webjars" % "bootstrap" % "2.3.1")
 
 play.Project.playJavaSettings
